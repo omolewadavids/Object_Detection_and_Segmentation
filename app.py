72cd09ca76bbf2530a89d3df84be70dba2623227
Omolewa Adaramola <omolewa.davids@gmail.com>
2022-06-18 00:29:37 -0400

    working on flask

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
+class test:
+    def __init__(self, fname, lname, mname):
+        self.fname = fname
+        self.lname = lname
+        self.mname = mname
 
 
 @app.route("/")
@@ -12,6 +19,12 @@ def hello_world():
 @app.route("/page")
 def page():
     return render_template("secondpage.html")
+
+
+@app.route("/pythonobj")
+def obj():
+    testing = test("Omolewa", "Adaramola", "Bukola")
+    return render_template("thirdpage.html", testing=testing)
 
 
 def main():
