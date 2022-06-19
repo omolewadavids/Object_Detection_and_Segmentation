38a9a8f3e1597c23532d63ff13d5bc0e2a394e20
Omolewa Adaramola <omolewa.davids@gmail.com>
2022-06-19 09:59:28 -0400

    updated app.py

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ from pymongo import MongoClient
 from common.utils import InitDBConnection as conn
 
 # TODO: how to use create_app function to avoid creating multiple mongodb connection
-app = Flask(__name__)
-
-client = MongoClient(conn.mongodb_connection())
-print([e for e in client.ODS.entries.find({})])
-client.ODS.entries.insert_one({'fname': 'Bukola', 'mname': 'Omolewa', 'lname': 'Adaramola'})
-app.db = client.ODS
 
 
 class Test:
@@ -19,23 +13,32 @@ class Test:
         self.mname = mname
 
 
-@app.route("/")
-def hello_world():
-    return render_template("firstpage.html")
+def create_app():
+    app = Flask(__name__)
 
+    client = MongoClient(conn.mongodb_connection())
+    print([e for e in client.ODS.entries.find({})])
+    client.ODS.entries.insert_one({'fname': 'Bukola', 'mname': 'Omolewa', 'lname': 'Adaramola'})
+    app.db = client.ODS
 
-@app.route("/page")
-def page():
-    return render_template("secondpage.html")
+    @app.route("/")
+    def hello_world():
+        return render_template("firstpage.html")
 
+    @app.route("/page")
+    def page():
+        return render_template("secondpage.html")
 
-@app.route("/pythonobj")
-def obj():
-    testing = Test("Omolewa", "Adaramola", "Bukola")
-    return render_template("thirdpage.html", testing=testing)
+    @app.route("/pythonobj")
+    def obj():
+        testing = Test("Omolewa", "Adaramola", "Bukola")
+        return render_template("thirdpage.html", testing=testing)
+
+    return app
 
 
 def main():
+    app = create_app()
     app.run(host='localhost', port=5000, debug=True)
 
 
